593fb81e13500d547bc81e45110b378de681224a
ISE-EXPLORELOGY <pasan@explorelogy.lk>
2023-09-28 07:07:44 +0530

    feat:contact-us page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useState } from "react"
 import Layout from "../components/layout"
 import Menu from "../components/menu"
 import { useStaticQuery, graphql } from "gatsby"
@@ -20,67 +20,26 @@ const Contact = () => {
     }
   `)
 
-  const education = [
-    {
-      id: 1,
-      year: "2021-2023",
-      degree: "Ph.D in Horriblensess ",
-      where: "- ABC University,Los Angeles, CA",
-    },
-    {
-      id: 2,
-      year: "2021-2023",
-      degree: "Ph.D in Horriblensess ",
-      where: "- ABC University,Los Angeles, CA",
-    },
-    {
-      id: 3,
-      year: "2021-2023",
-      degree: "Ph.D in Horriblensess ",
-      where: "- ABC University,Los Angeles, CA",
-    },
-    {
-      id: 4,
-      year: "2021-2023",
-      degree: "Ph.D in Horriblensess ",
-      where: "- ABC University,Los Angeles, CA",
-    },
-  ]
+  const [formData, setFormData] = useState({
+    name: "",
+    company: "",
+    email: "",
+    suggestions: "",
+  })
 
-  const experience = [
-    {
-      id: 1,
-      year: "2019 - Present",
-      profession: "Sr. Software Tester",
-      where: "Google Inc.",
-    },
-    {
-      id: 2,
-      year: "2019 - Present",
-      profession: "Sr. Software Tester",
-      where: "Google Inc.",
-    },
-    {
-      id: 3,
-      year: "2019 - Present",
-      profession: "Sr. Software Tester",
-      where: "Google Inc.",
-    },
-    {
-      id: 4,
-      year: "2019 - Present",
-      profession: "Sr. Software Tester",
-      where: "Google Inc.",
-    },
-  ]
+  const handleChange = e => {
+    const { name, value } = e.target
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
 
-  const resume = [
-    {
-      id: 1,
-      name: "Education",
-    },
-    { id: 2, name: "Experience" },
-  ]
+  const handleSubmit = e => {
+    e.preventDefault()
+    // You can add your form submission logic here
+    console.log("Form data submitted:", formData)
+  }
 
   return (
     <>
@@ -92,99 +51,125 @@ const Contact = () => {
                 <Menu />
               </div>
               <div className=" flex-cols justify-start items-start pt-40 ">
-                <div className="flex items-center gap-7">
-                  <Slide>
-                    <div className="flex items-center text-2xl text-white">
-                      Resume
-                    </div>
-                  </Slide>
-
-                  <div className="w-44 h-[1px] bg-gradient-to-r  from-red-400 to-pink-500" />
-                </div>
-
                 <Fade delay={1200} cascade damping={1e-1}>
-                  <div className=" text-lg mt-7 text-[#A6A6A6]">
-                    I'm Creative Director and UI/UX Designer from Sydney,
-                    Australia, working in web development and print media. I
-                    enjoy turning complex problems into simple, beautiful and
-                    intuitive designs.
-                  </div>
+                  <div className="text-2xl text-white"> Contact us</div>
                 </Fade>
-
                 <Fade delay={1400} cascade damping={1e-1}>
-                  <div className=" text-lg mt-7 text-[#A6A6A6]">
-                    My aim is to bring across your message and identity in the
-                    most creative way. I created web design for many famous
-                    brand companies.
-                  </div>
-                </Fade>
-                <Fade delay={1800} cascade damping={1e-1}>
-                  <div className=" text-2xl text-white mt-7">
-                    What I've done!
-                  </div>
-                </Fade>
-                <div className="grid grid-cols-2  gap-7   text-white justify-start items-center ">
-                  <Fade delay={1900} cascade damping={1e-1}>
-                    {resume.map((item, index) => (
-                      <div className="flex flex-row mt-7">
-                        <div
-                          key={index}
-                          className="flex justify-start item-center gap-3 "
-                        >
-                          <GatsbyImage
-                            image={getImage(
-                              data.allFile.nodes.find(
-                                node => node.name === item.name
-                              )
-                            )}
-                            alt={item.name}
-                            className="w-7"
-                          />
-                          <div>{item.name}</div>
-                        </div>
-                      </div>
-                    ))}
-                  </Fade>
-                </div>
-
-                <div className="grid grid-cols-2 grid-rows-2 gap-7  ">
-                  <div className="">
-                    {education.map((item, index) => (
-                      <Fade delay={2000} cascade damping={1e-1}>
-                        <div className="border border-[#212425] rounded-lg p-5 mt-7">
-                          <div class="flex flex-col ">
-                            <div class="justify-start  text-lg text-white  items-center">
-                              {item.year}
-                            </div>
-                            <div class="justify-start  text-lg text-white  items-center">
-                              {item.degree}
-                            </div>
-                            <div class="justify-start  text-[#A6A6A6]">
-                              {item.where}
-                            </div>
-                          </div>
-                        </div>
-                      </Fade>
-                    ))}
-                  </div>
                   <div>
-                    {experience.map((item, index) => (
-                      <Fade delay={2000} cascade damping={1e-1}>
-                        <div className="border border-[#212425] rounded-lg p-5 mt-7 ">
-                          <div class="flex flex-col">
-                            <div class="justify-start  text-lg text-white  items-center">
-                              {item.year}
-                            </div>
-                            <div class="justify-start  text-lg text-white  items-center">
-                              {item.profession}
-                            </div>
-                            <div class="justify-start text-[#A6A6A6]">
-                              {item.where}
-                            </div>
-                          </div>
-                        </div>
-                      </Fade>
-                    ))}
+                    <div class="mt-7 text-[#A6A6A6]">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Laudantium, modiaccusantium ipsum corporis quia asperiores
+                      dolorem nisi corrupti eveniet dolores ad maiores
+                      repellendus enim autem omnis fugiat perspiciatis? Ad,
+                      veritatis.
+                    </div>
+                    <div class="mt-7 text-[#A6A6A6]">
+                      New York, 94126, United States
+                    </div>
+                    <div class="mt-7 text-[#A6A6A6]">+ 01 234 567 89</div>
+                    <div class="mt-7 text-[#A6A6A6]">info@gmail.com</div>
+                  </div>
+                </Fade>
+
+                <div className="min-h-screen flex items-center justify-center">
+                  <div className=" shadow-md rounded mt-7 pb-8 w-112">
+                    <Fade delay={1600} cascade damping={1e-1}>
+                      <h2 className=" text-2xl text-white ">
+                        Leave somthing Here 😃 For me .!
+                      </h2>
+                    </Fade>
+
+                    <form onSubmit={handleSubmit}>
+                      <div className="mb-4 mt-7">
+                        <Fade delay={1700} cascade damping={1e-1}>
+                          <label
+                            className="block text-[#A6A6A6] text-sm font-bold mb-2"
+                            htmlFor="name"
+                          >
+                            Name
+                          </label>
+
+                          <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-[#A6A6A6] leading-tight focus:outline-none focus:shadow-outline"
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            placeholder="Your Name"
+                            required
+                          />
+                        </Fade>
+                      </div>
+                      <div className="mb-4">
+                        <Fade delay={1800} cascade damping={1e-1}>
+                          <label
+                            className="block text-[#A6A6A6] text-sm font-bold mb-2"
+                            htmlFor="company"
+                          >
+                            Company or Org
+                          </label>
+                          <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-[#A6A6A6] leading-tight focus:outline-none focus:shadow-outline"
+                            type="text"
+                            id="company"
+                            name="company"
+                            value={formData.company}
+                            onChange={handleChange}
+                            placeholder="Your Company or Organization"
+                          />
+                        </Fade>
+                      </div>
+                      <div className="mb-4">
+                        <Fade delay={1900} cascade damping={1e-1}>
+                          <label
+                            className="block text-[#A6A6A6] text-sm font-bold mb-2"
+                            htmlFor="email"
+                          >
+                            Email
+                          </label>
+                          <input
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-[#A6A6A6] leading-tight focus:outline-none focus:shadow-outline"
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            placeholder="Your Email"
+                            required
+                          />
+                        </Fade>
+                      </div>
+                      <div className="mb-4">
+                        <Fade delay={2000} cascade damping={1e-1}>
+                          <label
+                            className="block text-[#A6A6A6] text-sm font-bold mb-2"
+                            htmlFor="suggestions"
+                          >
+                            Suggestions
+                          </label>
+                          <textarea
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-[#A6A6A6] leading-tight focus:outline-none focus:shadow-outline h-32"
+                            id="suggestions"
+                            name="suggestions"
+                            value={formData.suggestions}
+                            onChange={handleChange}
+                            placeholder="Your Suggestions"
+                            required
+                          />
+                        </Fade>
+                      </div>
+                      <div className="flex items-center justify-center">
+                        <Fade delay={2100} cascade damping={1e-1}>
+                          <button
+                            className="bg-gradient-to-r mt-7 from-red-400 to-pink-500 text-white font-bold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline"
+                            type="submit"
+                          >
+                            Submit
+                          </button>
+                        </Fade>
+                      </div>
+                    </form>
                   </div>
                 </div>
               </div>
